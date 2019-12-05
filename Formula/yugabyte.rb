4dfd4682bd146ffce61c84a31669f32119cb7fd3
vp15591 <vp15591@mail.com>
2019-12-05 12:32:54 +0530

    python dependency added into yugabyte formula

--- Formula/yugabyte.rb
+++ Formula/yugabyte.rb
@@ -4,6 +4,7 @@ class Yugabyte < Formula
   url "https://downloads.yugabyte.com/yugabyte-2.0.6.0-darwin.tar.gz"
   sha256 "96c23560845647c3dbff4e1923f4a5caec5fb34ccb2e50fccee4c19d5671f3fc"
   depends_on :java => "1.8"
+  depends_on "python"
   def install
     libexec.install Dir["*"]
     bin.install_symlink libexec/"yugabyted"
