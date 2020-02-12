96f5de4aa9c5d39702d0b6a9c174353ac3938fa5
Sanketh I <sanketh@yugabyte.com>
2020-02-11 17:28:30 -0800

    Upgrade to 2.0.11.0

--- Formula/yugabytedb.rb
+++ Formula/yugabytedb.rb
@@ -1,8 +1,8 @@
 class Yugabytedb < Formula
   desc "High-performance distributed SQL database Yugabyte DB"
   homepage "https://yugabyte.com"
-  url "https://downloads.yugabyte.com/yugabyte-2.0.10.0-darwin.tar.gz"
-  sha256 "953de9bde56a327f8c1d3cea7633e4d2133c1f90fef7d39471a3d3ddd23f83a3"
+  url "https://downloads.yugabyte.com/yugabyte-2.0.11.0-darwin.tar.gz"
+  sha256 "39acba6800f9d4c665ed44323502cbb650d25430716cd0ecef7135b482d2ce7d"
 
   depends_on :java => "1.8"
   depends_on "python"
