35e517f7498786813cc523b40be275920482f742
Sanketh I <sanketh@yugabyte.com>
2020-02-28 13:17:46 -0800

    Point to client package

--- Formula/yugabytedb-client.rb
+++ Formula/yugabytedb-client.rb
@@ -1,8 +1,8 @@
 class YugabytedbClient < Formula
   desc "High-performance distributed SQL database Yugabyte DB"
   homepage "https://yugabyte.com"
-  url "https://downloads.yugabyte.com/yugabyte-2.1.0.0-darwin.tar.gz"
-  sha256 "cb451a47cc8a3804ed0152f4471b4c3626593da7b401d7bfbd843805beaa10f9" 
+  url "https://downloads.yugabyte.com/yugabyte-client-2.1.1.0-darwin.tar.gz"
+  sha256 "102a85f1ad9e02df46edeae0d2ef126aefd2b594e8a335284facf94d812dc8c3" 
   
   depends_on :java => "1.8"
   depends_on "python"
