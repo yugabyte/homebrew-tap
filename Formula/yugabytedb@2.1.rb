e4e4bdc7997fc8623fe6e405dc2e246b3ee91cf3
YugaByte CI <yugabyte-ci@users.noreply.github.com>
2020-09-09 08:03:37 +0000

    Update the version to 2.3.0.0

--- Formula/yugabytedb@2.1.rb
+++ Formula/yugabytedb@2.1.rb
@@ -1,8 +1,10 @@
-class Yugabytedb < Formula
+class YugabytedbAT21 < Formula
   desc "High-performance distributed SQL database Yugabyte DB"
   homepage "https://yugabyte.com"
-  url "https://downloads.yugabyte.com/yugabyte-2.3.0.0-darwin.tar.gz"
-  sha256 "624757a768befc169335be6f8a908b541befbd0bd9c9bf6049730c08bdb95426"
+  url "https://downloads.yugabyte.com/yugabyte-2.1.8.2-darwin.tar.gz"
+  sha256 "dd6cbd63ad4dd150c9707ed5dc8f3696adf9828dff941bae2255bc04eff7e924"
+
+  keg_only :versioned_formula
 
   depends_on :java => "1.8"
   depends_on "python"
