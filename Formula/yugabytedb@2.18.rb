862c83e1e4a6d0374ab20294954930daa344661f
Sanskar Garg <sgarg@yugabyte.com>
2024-01-30 15:27:59 +0530

    [brew][yugabytedb] Updating yugabytedb Formulae
    Summary:
    * Latest Stable: 2.20.1.3
    * 2.18.1.0 -> 2.18.5.2
    * 2.19.0.0 -> 2.19.3.0

--- Formula/yugabytedb@2.18.rb
+++ Formula/yugabytedb@2.18.rb
@@ -1,17 +1,17 @@
-class YugabytedbAT219 < Formula
-	desc "High-performance distributed SQL database Yugabyte DB"
+class YugabytedbAT218 < Formula
+    desc "High-performance distributed SQL database Yugabyte DB"
 	homepage "https://www.yugabyte.com/"
-	url "https://downloads.yugabyte.com/releases/2.19.3.0/yugabyte-2.19.3.0-b140-linux-x86_64.tar.gz"
-	version "2.19.3.0"
-	sha256 "77aa5bb02e118abffd7594e3aad3ae5ee06364a737cebd98fdac61a68d088d29"
-	license "Apache-2.0"
+    version "2.18.5.2"
+    url "https://downloads.yugabyte.com/releases/2.18.5.2/yugabyte-2.18.5.2-b1-linux-x86_64.tar.gz"
+    sha256 "84d34026734faa51e2d5a84c5c54e2d58f8cd44d77347af85cc6a1845e780ae0"
+    license "Apache-2.0"
 
 	keg_only :versioned_formula
 
 	depends_on "python@3.9"
 
-	def install
-		ENV.deparallelize
+    def install
+        ENV.deparallelize
 
 		libexec.install Dir["*"]
 		inreplace "#{libexec}/bin/yugabyted", "#!/usr/bin/env python", "#!/usr/bin/env python3"
@@ -22,8 +22,9 @@ class YugabytedbAT219 < Formula
 		bin.install_symlink libexec/"postgres/bin/ysqlsh" unless File.exist?(ysqlsh_symlink)
 		bin.install_symlink libexec/"bin/ycqlsh" unless File.exist?(ycqlsh_symlink)
 	end
-
-	test do
+  
+    test do
 		system "#{bin}/yugabyted", "version"
 	end
-end
+  end
+  
