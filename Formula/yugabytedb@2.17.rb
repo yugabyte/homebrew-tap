7a04f798881aefc33417b31574f73163cb41f689
Sanskar Garg <sgarg@yugabyte.com>
2024-03-19 16:09:10 +0530

    Updating Scripts to succesfully run brew audit. (#78)

--- Formula/yugabytedb@2.17.rb
+++ Formula/yugabytedb@2.17.rb
@@ -1,29 +1,29 @@
 class YugabytedbAT217 < Formula
-	desc "High-performance distributed SQL database Yugabyte DB"
-	homepage "https://www.yugabyte.com/"
-	url "https://downloads.yugabyte.com/releases/2.17.1.0/yugabyte-2.17.1.0-b439-darwin-x86_64.tar.gz"
-	version "2.17.1.0"
-	sha256 "b0831be893284f6e5652fe99dbd7aedb83894c838a8d2fda850d030b94afcaf2"
-	license "Apache-2.0"
+  desc "High-performance distributed SQL database Yugabyte DB"
+  homepage "https://www.yugabyte.com/"
+  url "https://downloads.yugabyte.com/releases/2.17.1.0/yugabyte-2.17.1.0-b439-darwin-x86_64.tar.gz"
+  version "2.17.1.0"
+  sha256 "b0831be893284f6e5652fe99dbd7aedb83894c838a8d2fda850d030b94afcaf2"
+  license "Apache-2.0"
 
-	keg_only :versioned_formula
+  keg_only :versioned_formula
 
-	depends_on "python@3.9"
+  depends_on "python@3.9"
 
-	def install
-		ENV.deparallelize
+  def install
+    ENV.deparallelize
 
-		libexec.install Dir["*"]
-		inreplace "#{libexec}/bin/yugabyted", "#!/usr/bin/env python", "#!/usr/bin/env python3"
+    libexec.install Dir["*"]
+    inreplace "#{libexec}/bin/yugabyted", "#!/usr/bin/env python", "#!/usr/bin/env python3"
 
-		bin.install_symlink libexec/"bin/yugabyted"
-		ysqlsh_symlink = "#{HOMEBREW_PREFIX}/bin/ysqlsh"
-		ycqlsh_symlink = "#{HOMEBREW_PREFIX}/bin/ycqlsh"
-		bin.install_symlink libexec/"postgres/bin/ysqlsh" unless File.exist?(ysqlsh_symlink)
-		bin.install_symlink libexec/"bin/ycqlsh" unless File.exist?(ycqlsh_symlink)
-	end
+    bin.install_symlink libexec/"bin/yugabyted"
+    ysqlsh_symlink = "#{HOMEBREW_PREFIX}/bin/ysqlsh"
+    ycqlsh_symlink = "#{HOMEBREW_PREFIX}/bin/ycqlsh"
+    bin.install_symlink libexec/"postgres/bin/ysqlsh" unless File.exist?(ysqlsh_symlink)
+    bin.install_symlink libexec/"bin/ycqlsh" unless File.exist?(ycqlsh_symlink)
+  end
 
-	test do
-		system "#{bin}/yugabyted", "version"
-	end
+  test do
+    system "#{bin}/yugabyted", "version"
+  end
 end
--- Formula/yugabytedb@2.19.rb
+++ Formula/yugabytedb@2.19.rb
@@ -1,29 +1,29 @@
 class YugabytedbAT219 < Formula
-	desc "High-performance distributed SQL database Yugabyte DB"
-	homepage "https://www.yugabyte.com/"
-	url "https://downloads.yugabyte.com/releases/2.19.3.0/yugabyte-2.19.3.0-b140-darwin-x86_64.tar.gz"
-	version "2.19.3.0"
-	sha256 "eac1257569176892d113034d5096d9b0bd75de14c94a3617c5b60f2f30b1e2c9"
-	license "Apache-2.0"
+  desc "High-performance distributed SQL database Yugabyte DB"
+  homepage "https://www.yugabyte.com/"
+  url "https://downloads.yugabyte.com/releases/2.19.3.0/yugabyte-2.19.3.0-b140-darwin-x86_64.tar.gz"
+  version "2.19.3.0"
+  sha256 "eac1257569176892d113034d5096d9b0bd75de14c94a3617c5b60f2f30b1e2c9"
+  license "Apache-2.0"
 
-	keg_only :versioned_formula
+  keg_only :versioned_formula
 
-	depends_on "python@3.9"
+  depends_on "python@3.9"
 
-	def install
-		ENV.deparallelize
+  def install
+    ENV.deparallelize
 
-		libexec.install Dir["*"]
-		inreplace "#{libexec}/bin/yugabyted", "#!/usr/bin/env python", "#!/usr/bin/env python3"
+    libexec.install Dir["*"]
+    inreplace "#{libexec}/bin/yugabyted", "#!/usr/bin/env python", "#!/usr/bin/env python3"
 
-		bin.install_symlink libexec/"bin/yugabyted"
-		ysqlsh_symlink = "#{HOMEBREW_PREFIX}/bin/ysqlsh"
-		ycqlsh_symlink = "#{HOMEBREW_PREFIX}/bin/ycqlsh"
-		bin.install_symlink libexec/"postgres/bin/ysqlsh" unless File.exist?(ysqlsh_symlink)
-		bin.install_symlink libexec/"bin/ycqlsh" unless File.exist?(ycqlsh_symlink)
-	end
+    bin.install_symlink libexec/"bin/yugabyted"
+    ysqlsh_symlink = "#{HOMEBREW_PREFIX}/bin/ysqlsh"
+    ycqlsh_symlink = "#{HOMEBREW_PREFIX}/bin/ycqlsh"
+    bin.install_symlink libexec/"postgres/bin/ysqlsh" unless File.exist?(ysqlsh_symlink)
+    bin.install_symlink libexec/"bin/ycqlsh" unless File.exist?(ycqlsh_symlink)
+  end
 
-	test do
-		system "#{bin}/yugabyted", "version"
-	end
+  test do
+    system "#{bin}/yugabyted", "version"
+  end
 end
