dff1aa0412fbe261d16a557f6c2d5b2b4f2f62be
Shivansh Gahlot <42472145+ShivanshGahlot@users.noreply.github.com>
2024-07-04 12:20:15 +0530

    Added yb-voyager@1.7.2 and debezium@2.3.3-1.7.2 formula files and updated aliases (#90)

---
+++ Formula/yb-voyager@1.7.2.rb
@@ -0,0 +1,32 @@
+
+                    class YbVoyagerAT172 < Formula
+                        desc "YugabyteDB's migration tool"
+                        homepage "https://github.com/yugabyte/yb-voyager/"
+                        url "https://github.com/yugabyte/yb-voyager/archive/refs/tags/yb-voyager/v1.7.2.tar.gz"
+                        sha256 "16ebe7dee1593420df242c18ce5a1cbdac3b00c39b65c5da0441a8dabd3961cd"
+                        version "1.7.2"
+                        license "Apache-2.0"
+                        depends_on "go" => :build
+                        depends_on "libpq"
+                        depends_on "sqlite"
+                        depends_on "yugabyte/tap/debezium@2.3.3-1.7.2"
+                        
+                        def install
+                            ENV.deparallelize
+                            Dir.chdir("yb-voyager") do
+                                system "go", "build"
+                                bin.install "yb-voyager"
+                            end
+                            Dir.chdir("yb-voyager/src/srcdb/data") do
+                                (prefix/"etc/").mkdir
+                                (prefix/"etc/yb-voyager/").mkdir
+                                cp_r "pg_dump-args.ini", prefix/"etc/yb-voyager/pg_dump-args.ini"
+                                cp_r "gather-assessment-metadata", prefix/"etc/yb-voyager/"
+                            end
+                        end
+
+                        test do
+                            system "#{bin}/yb-voyager", "version"
+                        end
+                    end
+                    
