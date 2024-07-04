daca7937ffb50e6b9e50904e137d9e4ac6676387
Shivansh Gahlot <42472145+ShivanshGahlot@users.noreply.github.com>
2024-07-04 13:41:26 +0530

    Fixed link in debezium 1.7.2 file (#93)

--- Formula/debezium@2.3.3-1.7.2.rb
+++ Formula/debezium@2.3.3-1.7.2.rb
@@ -1,7 +1,7 @@
 class DebeziumAT233172 < Formula
     desc "Debezium is an open source distributed platform for change data capture"
     homepage "https://github.com/yugabyte/yb-voyager/"
-    url "https://github.com/yugabyte/yb-voyager/releases/download/yb-voyager%2F1.7.2/debezium-server.tar.gz"
+    url "https://github.com/yugabyte/yb-voyager/releases/download/yb-voyager%2Fv1.7.2/debezium-server.tar.gz"
     version "2.3.3-1.7.2"
     sha256 "2fafe1c0dc9401528685b7d9a25c9a9872adee437df0ddd9dcf3687d997cb8e0"
     license "Apache-2.0"
