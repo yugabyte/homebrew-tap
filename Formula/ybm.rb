4461bc5e449bfd36cc28286a337ca324e0f5b3d8
Srinivas Pothuraju <posriniv@gmail.com>
2024-11-07 22:24:31 -0600

    update cli version

--- Formula/ybm.rb
+++ Formula/ybm.rb
@@ -1,21 +1,21 @@
 class Ybm < Formula
   desc "YugabyteDB Managed CLI"
   homepage "https://github.com/yugabyte/ybm-cli"
-  version "0.1.20"
+  version "0.1.21"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.20/ybm_0.1.20_darwin_amd64.zip"
-      sha256 "2e436677852dbba6943e38ac8483fda2bb1247c4c4ae0208c0815c325d7a5634"
+      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.21/ybm_0.1.21_darwin_amd64.zip"
+      sha256 "bc87ff7cdf461468282f70955e72c7b8c081a3b5344af23468a838ff855e009a"
 
       def install
         bin.install "ybm"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.20/ybm_0.1.20_darwin_arm64.zip"
-      sha256 "f4ef71bdd805027708d65e0016ebf727d15c023c40bc7a68ca57b58153b05c03"
+      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.21/ybm_0.1.21_darwin_arm64.zip"
+      sha256 "ab2a594a433ba9d9504786d715e33803ac67398e7f3f52b1c209122af7341365"
 
       def install
         bin.install "ybm"
@@ -25,16 +25,16 @@ class Ybm < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.20/ybm_0.1.20_linux_amd64.zip"
-      sha256 "19bb72a468840f2b29ebef403dccb5f2f2851e00a01bc853fd73cc8e40724dee"
+      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.21/ybm_0.1.21_linux_amd64.zip"
+      sha256 "b38bcbc3f3d776f869f30421f4f65a2db26332a5cc18b54e96c049dae5872f5a"
 
       def install
         bin.install "ybm"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.20/ybm_0.1.20_linux_arm64.zip"
-      sha256 "132be6dc2d6a4950e88367a0d49c88d95935f17f5fa4addc14b7580908090465"
+      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.21/ybm_0.1.21_linux_arm64.zip"
+      sha256 "0fa12bdbb7fbbbc1a44a471885fa88591cf777fba4f073c716bb7e3c9128fb40"
 
       def install
         bin.install "ybm"
---
+++ Formula/ybm@0.1.21.rb
@@ -0,0 +1,44 @@
+class Ybm < Formula
+    desc "YugabyteDB Managed CLI"
+    homepage "https://github.com/yugabyte/ybm-cli"
+    version "0.1.21"
+    license "Apache-2.0"
+
+    on_macos do
+      if Hardware::CPU.intel?
+        url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.21/ybm_0.1.21_darwin_amd64.zip"
+        sha256 "bc87ff7cdf461468282f70955e72c7b8c081a3b5344af23468a838ff855e009a"
+
+        def install
+          bin.install "ybm"
+        end
+      end
+      if Hardware::CPU.arm?
+        url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.21/ybm_0.1.21_darwin_arm64.zip"
+        sha256 "ab2a594a433ba9d9504786d715e33803ac67398e7f3f52b1c209122af7341365"
+
+        def install
+          bin.install "ybm"
+        end
+      end
+    end
+
+    on_linux do
+      if Hardware::CPU.intel?
+        url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.21/ybm_0.1.21_linux_amd64.zip"
+        sha256 "b38bcbc3f3d776f869f30421f4f65a2db26332a5cc18b54e96c049dae5872f5a"
+
+        def install
+          bin.install "ybm"
+        end
+      end
+      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+        url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.21/ybm_0.1.21_linux_arm64.zip"
+        sha256 "0fa12bdbb7fbbbc1a44a471885fa88591cf777fba4f073c716bb7e3c9128fb40"
+
+        def install
+          bin.install "ybm"
+        end
+      end
+    end
+  end  
