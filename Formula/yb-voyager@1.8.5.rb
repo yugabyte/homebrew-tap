bd03d42e1bb4d9fa1ec597dfef696b4730206a96
Yugabyte CI <yugabyte@gmail.com>
2024-11-12 11:45:22 +0000

    Added yb-voyager@1.8.5 and debezium@2.5.2-1.8.5 formula files and updated aliases

---
+++ Formula/yb-voyager@1.8.5.rb
@@ -0,0 +1,36 @@
+class YbVoyagerAT185 < Formula
+    desc "YugabyteDB's migration tool"
+    homepage "https://github.com/yugabyte/yb-voyager/"
+    url "https://github.com/yugabyte/yb-voyager/archive/refs/tags/yb-voyager/v1.8.5.tar.gz"
+    sha256 "db8c68c8f53c5fb94bdcd226bb497ee025b409eb8e4065b0fe9ce4fec3be5138"
+    version "1.8.5"
+    license "Apache-2.0"
+    depends_on "go" => :build
+    depends_on "postgresql@16"
+    depends_on "sqlite"
+    depends_on "yugabyte/tap/debezium@2.5.2-1.8.5"
+    
+    def install
+        ENV.deparallelize
+        Dir.chdir("yb-voyager") do
+            system "go", "build"
+            bin.install "yb-voyager"
+        end
+        Dir.chdir("yb-voyager/src/srcdb/data") do
+            (prefix/"etc/").mkdir
+            (prefix/"etc/yb-voyager/").mkdir
+            cp_r "pg_dump-args.ini", prefix/"etc/yb-voyager/pg_dump-args.ini"
+            cp_r "gather-assessment-metadata", prefix/"etc/yb-voyager/"
+        end
+        Dir.chdir("guardrails-scripts") do
+            (prefix/"opt/").mkdir
+            (prefix/"opt/yb-voyager").mkdir
+            (prefix/"opt/yb-voyager/guardrails-scripts").mkdir
+            cp_r ".", prefix/"opt/yb-voyager/guardrails-scripts"
+        end
+    end
+
+    test do
+        system "#{bin}/yb-voyager", "version"
+    end
+end
