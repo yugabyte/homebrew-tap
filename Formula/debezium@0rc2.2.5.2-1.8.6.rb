98bca0b47129b4301b349977b8d6479f2d9ac1e4
Shivansh Gahlot <42472145+ShivanshGahlot@users.noreply.github.com>
2024-11-25 19:47:00 +0530

    Fixed classname in rc2 release for 1.8.6 (#116)

--- Formula/debezium@0rc2.2.5.2-1.8.6.rb
+++ Formula/debezium@0rc2.2.5.2-1.8.6.rb
@@ -1,4 +1,4 @@
-class DebeziumAT0Rc2252186 < Formula
+class DebeziumAT0rc2252186 < Formula
     desc "Debezium is an open source distributed platform for change data capture"
     homepage "https://github.com/yugabyte/yb-voyager/"
     url "https://github.com/yugabyte/yb-voyager/releases/download/yb-voyager%2Fv0rc2.1.8.6/debezium-server.tar.gz"
