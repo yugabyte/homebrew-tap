4e9add04b70b27ecc6a6bba0dbe40e9bdaf949a9
Yugabyte CI <yugabyte@gmail.com>
2024-12-10 08:13:31 +0000

    Added yb-voyager@1.8.7 and debezium@2.5.2-1.8.7 formula files and updated aliases

--- Formula/debezium@2.5.2-1.8.7.rb
+++ Formula/debezium@2.5.2-1.8.7.rb
@@ -1,9 +1,9 @@
-class DebeziumAT0rc1252187 < Formula
+class DebeziumAT252187 < Formula
     desc "Debezium is an open source distributed platform for change data capture"
     homepage "https://github.com/yugabyte/yb-voyager/"
-    url "https://github.com/yugabyte/yb-voyager/releases/download/yb-voyager%2Fv0rc1.1.8.7/debezium-server.tar.gz"
-    version "0rc1.2.5.2-1.8.7"
-    sha256 "bdb422a9d4f5ab7b9b58c781aafb09ffbf313277c0ec73d78a596c9814a8fb3f"
+    url "https://github.com/yugabyte/yb-voyager/releases/download/yb-voyager%2Fv1.8.7/debezium-server.tar.gz"
+    version "2.5.2-1.8.7"
+    sha256 "8b16cddf587ff43b106c3603a95a6d40d76a3ad2cbc6ff59aca4893d7f80739d"
     license "Apache-2.0"
     depends_on "maven" => :build
     depends_on "java11" => :build
