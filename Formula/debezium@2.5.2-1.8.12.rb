aa590928155303a0c289d7873d9840c00125419b
Aneesh Makala <amakala@yugabyte.com>
2025-02-25 19:48:37 +0530

    Added yb-voyager@1.8.12 and debezium@2.5.2-1.8.12 formula files and updated aliases (#139)
    Co-authored-by: Yugabyte CI <yugabyte@gmail.com>

--- Formula/debezium@2.5.2-1.8.12.rb
+++ Formula/debezium@2.5.2-1.8.12.rb
@@ -3,7 +3,7 @@ class DebeziumAT2521812 < Formula
     homepage "https://github.com/yugabyte/yb-voyager/"
     url "https://github.com/yugabyte/yb-voyager/releases/download/yb-voyager%2Fv1.8.12/debezium-server.tar.gz"
     version "2.5.2-1.8.12"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "6254c8eb035c2e1bb3a951d9c2ce416da749cbb55d5614ccb99b491246245666"
     license "Apache-2.0"
     depends_on "maven" => :build
     depends_on "java11" => :build
