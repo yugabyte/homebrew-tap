32a022d80aebbcbfcbfed7327673b2ec3924e10c
YugaByte CI <yugabyte-ci@users.noreply.github.com>
2025-03-21 20:35:29 +0000

    Update the version to 2.25.1.0

---
+++ Formula/yugabytedb@2.25.rb
@@ -0,0 +1,37 @@
+class YugabytedbAT225 < Formula
+  desc "High-performance distributed SQL database Yugabyte DB"
+  homepage "https://www.yugabyte.com/"
+  url "https://downloads.yugabyte.com/releases/2.25.1.0/yugabyte-2.25.1.0-b381-darwin-x86_64.tar.gz"
+  version "2.25.1.0"
+  sha256 "2a176646adfc9cae88f89d8641547f397d11df898729c930dd326c16edcf17c7"
+  license "Apache-2.0"
+
+  keg_only :versioned_formula
+
+  depends_on "python@3.9"
+
+  def install
+    ENV.deparallelize
+
+    libexec.install Dir["*"]
+
+    bin.install_symlink libexec/"bin/yugabyted"
+    ysqlsh_symlink = "#{HOMEBREW_PREFIX}/bin/ysqlsh"
+    ycqlsh_symlink = "#{HOMEBREW_PREFIX}/bin/ycqlsh"
+
+    ysql_dump_symlink = "#{HOMEBREW_PREFIX}/bin/ysql_dump"
+    ysql_dumpall_symlink = "#{HOMEBREW_PREFIX}/bin/ysql_dumpall"
+    ysql_bench_symlink = "#{HOMEBREW_PREFIX}/bin/ysql_bench"
+
+    bin.install_symlink libexec/"postgres/bin/ysqlsh" unless File.exist?(ysqlsh_symlink)
+    bin.install_symlink libexec/"bin/ycqlsh" unless File.exist?(ycqlsh_symlink)
+
+    bin.install_symlink libexec/"postgres/bin/ysql_dump" unless File.exist?(ysql_dump_symlink)
+    bin.install_symlink libexec/"postgres/bin/ysql_dumpall" unless File.exist?(ysql_dumpall_symlink)
+    bin.install_symlink libexec/"postgres/bin/ysql_bench" unless File.exist?(ysql_bench_symlink)
+  end
+
+  test do
+    system "#{bin}/yugabyted", "version"
+  end
+end
