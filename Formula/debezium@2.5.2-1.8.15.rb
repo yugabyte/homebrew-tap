13fcd5be34f2757395e94026246ab439496371e8
Shivansh Gahlot <42472145+ShivanshGahlot@users.noreply.github.com>
2025-04-08 18:19:55 +0530

    Added yb-voyager@1.8.15 and debezium@2.5.2-1.8.15 formula files and updated aliases (#147)

--- Formula/debezium@2.5.2-1.8.15.rb
+++ Formula/debezium@2.5.2-1.8.15.rb
@@ -1,9 +1,9 @@
-class DebeziumAT0rc12521815 < Formula
+class DebeziumAT2521815 < Formula
     desc "Debezium is an open source distributed platform for change data capture"
     homepage "https://github.com/yugabyte/yb-voyager/"
-    url "https://github.com/yugabyte/yb-voyager/releases/download/yb-voyager%2Fv0rc1.1.8.15/debezium-server.tar.gz"
-    version "0rc1.2.5.2-1.8.15"
-    sha256 "e69b1377b4bc19318899ff09fa223e06657ac567046f45fc0897c0dafc0ac03d"
+    url "https://github.com/yugabyte/yb-voyager/releases/download/yb-voyager%2Fv1.8.15/debezium-server.tar.gz"
+    version "2.5.2-1.8.15"
+    sha256 "4a05e1e1b41f558f80e3d9b9061707c3d4a44b21b3df033ade756843bbdbdbb2"
     license "Apache-2.0"
     depends_on "maven" => :build
     depends_on "java11" => :build
--- Formula/yb-voyager@1.8.15.rb
+++ Formula/yb-voyager@1.8.15.rb
@@ -1,14 +1,14 @@
-class YbVoyagerAT0rc11815 < Formula
+class YbVoyagerAT1815 < Formula
     desc "YugabyteDB's migration tool"
     homepage "https://github.com/yugabyte/yb-voyager/"
-    url "https://github.com/yugabyte/yb-voyager/archive/refs/tags/yb-voyager/v0rc1.1.8.15.tar.gz"
-    sha256 "4fb9792a597456f6466d406f3affe3d0cd49055d3351677d7b53d8f3d530b232"
-    version "0rc1.1.8.15"
+    url "https://github.com/yugabyte/yb-voyager/archive/refs/tags/yb-voyager/v1.8.15.tar.gz"
+    sha256 "81f227bc6354ee236e4eb4c1aad9595a13fd09342a4c57e5867bf6c04e9d033d"
+    version "1.8.15"
     license "Apache-2.0"
     depends_on "go" => :build
     depends_on "postgresql@17"
     depends_on "sqlite"
-    depends_on "yugabyte/tap/debezium@0rc1.2.5.2-1.8.15"
+    depends_on "yugabyte/tap/debezium@2.5.2-1.8.15"
     
     def install
         ENV.deparallelize
