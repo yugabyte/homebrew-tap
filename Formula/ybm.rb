9f1ec66fbba6d43db1d3d0813274ff80ebe3cb8d
Srinivas Pothuraju <posriniv@gmail.com>
2025-05-12 13:02:04 -0500

    release new version of the CLI

--- Formula/ybm.rb
+++ Formula/ybm.rb
@@ -1,21 +1,21 @@
 class Ybm < Formula
   desc "YugabyteDB Managed CLI"
   homepage "https://github.com/yugabyte/ybm-cli"
-  version "0.1.24"
+  version "0.1.25"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.24/ybm_0.1.24_darwin_amd64.zip"
-      sha256 "7a1090a3f508f281ead7c679df3e5ce1d48829ac7b873388b6b0e21a80835910"
+      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.25/ybm_0.1.25_darwin_amd64.zip"
+      sha256 "ffde7935f6c75c6b841d9399b534a299bb9e83e1a4a25df8734f1d6a7f2d3c7a"
 
       def install
         bin.install "ybm"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.24/ybm_0.1.24_darwin_arm64.zip"
-      sha256 "1c5563f506eff2b16788ba201a71837c8156ea10e08d0c27c5d4275d533b0b2d"
+      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.25/ybm_0.1.25_darwin_arm64.zip"
+      sha256 "793140f3ec9df7ccb15fe674616569a959030ec58eba5e33b51b3023c55ab5fd"
 
       def install
         bin.install "ybm"
@@ -25,16 +25,16 @@ class Ybm < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.24/ybm_0.1.24_linux_amd64.zip"
-      sha256 "2e7afb9be7afdf33aeeae15ceb49c77f6ce997cf3fec2688d62387e22a6fd4b6"
+      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.25/ybm_0.1.25_linux_amd64.zip"
+      sha256 "17bdcbf6b6ba850f4b7c4ce5b46b3fa2e4cc98b1aaae55a0cad7745b28325ea9"
 
       def install
         bin.install "ybm"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.24/ybm_0.1.24_linux_arm64.zip"
-      sha256 "7e704f78cd651c81d2815f56c72e86248294f5ef8dc4a2d300bb65dafb4a4f1a"
+      url "https://github.com/yugabyte/ybm-cli/releases/download/v0.1.25/ybm_0.1.25_linux_arm64.zip"
+      sha256 "04d86095ed6ce44aafe032531495fd3de98cc7aebe4332828f931594c62234ed"
 
       def install
         bin.install "ybm"
