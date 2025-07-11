956b1aca392c630fd5604c99aa0c24488eaf6737
Shivansh Gahlot <42472145+ShivanshGahlot@users.noreply.github.com>
2025-07-11 15:35:24 +0530

    Added yb-voyager@0rc1.2025.7.2 and debezium@0rc1.2.5.2-0rc1.2025.7.2 formula files and updated aliases (#161)

---
+++ Formula/debezium@0rc1.2.5.2-2025.7.2.rb
@@ -0,0 +1,16 @@
+class DebeziumAT0rc1252202572 < Formula
+    desc "Debezium is an open source distributed platform for change data capture"
+    homepage "https://github.com/yugabyte/yb-voyager/"
+    url "https://github.com/yugabyte/yb-voyager/releases/download/yb-voyager%2Fv0rc1.2025.7.2/debezium-server.tar.gz"
+    version "0rc1.2.5.2-2025.7.2"
+    sha256 "87ae60b51d1bc1cc4fecd8993c3419674c094843b73edbaca28f80daa8452678"
+    license "Apache-2.0"
+    depends_on "maven" => :build
+    depends_on "java11" => :build
+
+    def install
+        ENV.deparallelize
+        (prefix/"debezium-server").mkdir
+        cp_r ".", prefix/"debezium-server"
+    end
+end
