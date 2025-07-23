979b87aad2eb3dc20686254dee4e04b0f897587e
YugaByte CI <yugabyte-ci@users.noreply.github.com>
2025-07-23 17:14:29 +0000

    Update the version to 2025.1.0.0

--- Formula/yugabytedb@2024.1.rb
+++ Formula/yugabytedb@2024.1.rb
@@ -1,10 +1,12 @@
-class Yugabytedb < Formula
+class YugabytedbAT20241 < Formula
   desc "High-performance distributed SQL database Yugabyte DB"
   homepage "https://www.yugabyte.com/"
-  url "https://downloads.yugabyte.com/releases/2025.1.0.0/yugabyte-2025.1.0.0-b168-darwin-x86_64.tar.gz"
-  version "2025.1.0.0"
-  sha256 "6e10a229c1733573358eb044f74bb6d6c7300ff7d0bd1dc7958fd920478f6667"
+  url "https://downloads.yugabyte.com/releases/2024.1.3.1/yugabyte-2024.1.3.1-b8-darwin-x86_64.tar.gz"
+  version "2024.1.3.1"
+  sha256 "1588e340ef1f0c212c99a322209917c4be3c8988adddf05d20881224b1e15d06"
   license "Apache-2.0"
+
+  keg_only :versioned_formula
 
   depends_on "python@3.9"
 
