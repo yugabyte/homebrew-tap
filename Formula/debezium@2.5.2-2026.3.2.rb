687764dc5963836ed651152309bc70c2fc77a472
YugaByte CI <yugabyte-ci@users.noreply.github.com>
2026-03-17 17:46:04 +0530

    Added yb-voyager@2026.3.2 and debezium@2.5.2-2026.3.2 formula files and updated aliases (#219)
    Co-authored-by: Yugabyte CI <yugabyte@gmail.com>

--- Formula/debezium@2.5.2-2026.3.2.rb
+++ Formula/debezium@2.5.2-2026.3.2.rb
@@ -1,9 +1,9 @@
-class DebeziumAT0rc2252202632 < Formula
+class DebeziumAT252202632 < Formula
     desc "Debezium is an open source distributed platform for change data capture"
     homepage "https://github.com/yugabyte/yb-voyager/"
-    url "https://github.com/yugabyte/yb-voyager/releases/download/yb-voyager%2Fv0rc2.2026.3.2/debezium-server.tar.gz"
-    version "0rc2.2.5.2-2026.3.2"
-    sha256 "63397ec6ae318e9af2c53231b52292b60ddeebd33e923578ed8f6e23ea6e1e76"
+    url "https://github.com/yugabyte/yb-voyager/releases/download/yb-voyager%2Fv2026.3.2/debezium-server.tar.gz"
+    version "2.5.2-2026.3.2"
+    sha256 "a6ae7dbf215260c45b7e28954d817cc96c899bb2dcba552733a4272254f5aba6"
     license "Apache-2.0"
 
     def install
